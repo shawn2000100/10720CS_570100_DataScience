28ebbfeb86a63c7227dabd9fed24dab3773248de
shawn2000100 <shawn2000100@gmail.com>
2019-06-22 00:40:24 +0800

    Add files via upload

--- DataScience_Final/crawler.py
+++ DataScience_Final/crawler.py
@@ -2,67 +2,66 @@ import sys
 import os
 import json
 import time
-from urllib.parse import quote
-from urllib.request import urlopen, Request # 另一種爬法
+from datetime import datetime
+from urllib.parse import quote, unquote
+from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 
 
-def crawl_news(keyword, mode):
-    # 第一步先針對關鍵字來建立資料夾
-    newdir = keyword + '_0608'
+# 針對關鍵字在Google News上爬蟲，並存進資料夾裡
+def crawl_news(search_keyword, language_mode):
+    # 針對關鍵字建立資料夾
+    today = str(datetime.today().strftime('%m%d'))
+    newdir = search_keyword + '_' + today
     if not os.path.exists(newdir): # 判斷資料夾是否存在
         os.makedirs(newdir)        # 建立資料夾
 
     # 判斷是中文還是英文爬蟲，因為到時候要編碼
-    if mode == 'ch':
-        keyword = quote(keyword.encode('utf-8')) # 要用中文爬蟲比較麻煩，須要特別編碼一下!
-    elif mode != 'ch' and sys.argv[2] != 'en':
-        print('你應該輸入錯格式了!')
+    if language_mode == 'ch':
+        search_keyword = quote(search_keyword.encode('utf-8'))  # 要用中文爬蟲比較麻煩，須要特別編碼一下!，這個quote有它的用意，是給url用的
+    elif language_mode != 'ch' and language_mode != 'en':
+        print('輸入的格式應該有誤...')
+        print('範例: python crawler.py 學歷通膨 ch')
+        print('範例: python crawler.py degree+inflation en')
         return
 
-    # 開始Google爬蟲
+    # 開始Google爬蟲，因為每個網址都長得不太一樣，所以之後我要用迴圈慢慢換網址來爬
     # 這是第一頁用的網址
-    url1 = 'https://www.google.com.tw/search?q=' + keyword + '&num=100&source=lnms&tbm=nws&sa=X&ved=0ahUKEwjM0uqM3MjiAhVKHKYKHaPIAEcQ_AUIECgB&biw=1536&bih=754'
+    page1_url = 'https://www.google.com.tw/search?q=' + search_keyword + '&num=100&source=lnms&tbm=nws&sa=X&ved=0ahUKEwjM0uqM3MjiAhVKHKYKHaPIAEcQ_AUIECgB&biw=1536&bih=754'
     # 這是第二頁用的網址
-    url2 =  'https://www.google.com.tw/search?q=' + keyword + '&num=100&tbm=nws&ei=AaryXMrQMILVmAXx2ZPoCA&start=100&sa=N&ved=0ahUKEwiK1eeU3MjiAhWCKqYKHfHsBI0Q8NMDCOIC&biw=1536&bih=754&dpr=1.25'
+    page2_url =  'https://www.google.com.tw/search?q=' + search_keyword + '&num=100&tbm=nws&ei=AaryXMrQMILVmAXx2ZPoCA&start=100&sa=N&ved=0ahUKEwiK1eeU3MjiAhWCKqYKHfHsBI0Q8NMDCOIC&biw=1536&bih=754&dpr=1.25'
     # 這是第三用的網址
-    url3 = 'https://www.google.com.tw/search?q=' + keyword + '&num=100&tbm=nws&ei=zqvyXL_ZKMiT8wWMx5zoDA&start=200&sa=N&ved=0ahUKEwi_-Mjw3cjiAhXIybwKHYwjB804ZBDw0wMI2AI&biw=1536&bih=754&dpr=1.25'
-
-    # 標明自己身分，可以從瀏覽器的 inspect => request header 來看
+    page3_url = 'https://www.google.com.tw/search?q=' + search_keyword + '&num=100&tbm=nws&ei=zqvyXL_ZKMiT8wWMx5zoDA&start=200&sa=N&ved=0ahUKEwi_-Mjw3cjiAhXIybwKHYwjB804ZBDw0wMI2AI&biw=1536&bih=754&dpr=1.25'
+    # 標明自己的身分很重要!，可以從瀏覽器的 inspect => request header 來看
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
-    # 這是拿來存所有爬蟲結果的
-    search_result = list()
 
-    # 因為每個網址都長得不太一樣，所以我用迴圈慢慢換網址來爬
-    for url in [url1, url2, url3]:
-        req = Request(url=url, headers=headers)
+    search_result = list() # 這是拿來存所有爬蟲結果的
+    for page_url in [page1_url, page2_url, page3_url]:
+        req = Request(url=page_url, headers=headers)
         html = urlopen(req).read()
         soup = BeautifulSoup(html, "html.parser")
 
         # 開始找要的資料
         items = soup.findAll("div", {"class": "g"})
         for item in items:
-            # title
             news_title = item.find("h3", {"class": "r"}).find("a").text
-            # url
-            news_link = item.find("h3", {"class": "r"}).find("a")['href'] # 或者 item.find("h3", {"class": "r"}).find("a").get("href")
-            # content
+            news_link = item.find("h3", {"class": "r"}).find("a")['href'] # 或者 item.find("h3", {"class": "r"}).find("a").get("href") 也可以
             news_text = item.find("div", {"class": "st"}).text
-
-            # add item into json object
+            # 把爬到的新聞標題、鏈結、摘要 存進一個dict，類似JSON格式
             search_result.append({
                 "news_title": news_title,
                 "news_link": news_link,
                 "news_text": news_text
             })
 
-        print("page %s 完成" % url)
-        time.sleep(5) # 爬完很累，睡5秒
-    # print(search_result)
+        print("--- page: %s 完成" % page_url)
+        time.sleep(5) # 睡覺很重要，不然會被當成DDOS ban掉
 
-    # 準備輸出檔案 degree+inflation_0602
-    with open(newdir + '/' + 'search_result.json', 'w') as output_file:
+    # print(search_result)
+    # 輸出檔案到新增的資料夾 keywords_DDMM/keywords.json
+    with open(newdir + '/' + unquote(search_keyword) + '.json', 'w') as output_file:
         json.dump(search_result, output_file, indent=4)
+
 
 if __name__ == '__main__':
     # 不要直接開啟主程式，於命令列開啟
@@ -72,9 +71,11 @@ if __name__ == '__main__':
         print('範例: python crawler.py degree+inflation en')
     # 在命令列打關鍵字爬蟲
     elif len(sys.argv) == 3:
-        crawl_news(sys.argv[1], sys.argv[2])
-        print('----- crawler finished -----')
+        search_keyword, language_mode = sys.argv[1], sys.argv[2]
+        print('--- 開始爬蟲: ' + search_keyword + ' ---')
+        crawl_news(search_keyword, language_mode)
+        print('--- 爬蟲完成 ---')
     else:
-        print('輸入的格式應該有誤')
+        print('輸入的格式應該有誤...')
         print('範例: python crawler.py 學歷通膨 ch')
         print('範例: python crawler.py degree+inflation en')
--- DataScience_Final/show_data.py
+++ DataScience_Final/show_data.py
@@ -1,110 +1,103 @@
 #encoding=utf-8
-import sys
 import os
 import json
-from apyori import apriori
-import jieba
-from langconv import*  # 繁體轉簡體用，為了讓 結巴 的中文斷詞表現更好
-import numpy as np
-import matplotlib.pyplot as plt
+from apyori import apriori  # Frequent Pattern 分析用
+import jieba                # 中文斷詞用
+from langconv import*       # 繁體轉簡體用，為了讓 結巴 的中文斷詞表現更好
 from wordcloud import WordCloud
 
-# 有些字我們不在乎
+# 有些字我們不在乎，只要一碰到就可以去掉它...
 stop_word_list = [', ', ',', '...', '的', '\xa0', ' ', '「', '」', '。', '，', '、', '（', '）', '：', '.', '...', '】', '【', '....', '”',
                   'a', 'is', 'of', 'the', 'an', 'to', 'as', 'at', 'by', 'in', 'on', 'with', 'for', 'and', 'are', 'that', 'have', 'has', 'had', 'we',
                   '-', '—', 'your', 'its', '', '“', '／', '；', '》', '《', '“', 'he', 'they', 'was', 'or', 'be', 'this', 'how', 'his', 'per',
                   'it\'s', 'so', 'who', 'were', 'their', 'it', 'what', 'you', 'our', 'she', 'my', 'say', 10, 20]
 
-
-# 將所有爬到的新聞們整合成一份 txt 檔案，以便將來討論某個特定關鍵字的成因
-def aggregate_news_en(json_file):
-    with open('aggregated_news_en.txt', 'w') as f:
-        for data in json_file:
-            f.write('news_title: ')
-            f.write(str(data['news_title'].encode(encoding="utf-8")))
-            f.write('\n')
-            f.write('news_text: ')
-            f.write(str(data['news_text'].encode(encoding="utf-8")))
-            f.write('\n')
-            f.write('news_link: ')
-            f.write(str(data['news_link'].encode(encoding="utf-8")))
-            f.write('\n\n')
-
-
-# 將所有爬到的新聞們整合成一份 txt 檔案，中文版需處理編碼問題，比較麻煩
-def aggregate_news_ch(json_file):
-    with open('aggregated_news_ch.txt', 'wb') as f:
-        for data in json_file:
-            f.write('news_title: '.encode('utf8'))
-            f.write(data['news_title'].encode('utf8'))
-            f.write('\n'.encode('utf8'))
-            f.write('news_text: '.encode('utf8'))
-            f.write(data['news_text'].encode(encoding="utf-8"))
-            f.write('\n'.encode('utf8'))
-            f.write('news_link: '.encode('utf8'))
-            f.write(data['news_link'].encode(encoding="utf-8"))
-            f.write('\n\n'.encode('utf8'))
+# 讀取 英文 版本JSON檔，並將所有檔案整合成一份 txt 檔並輸出，接著再做資料處理  (老實說，模組化寫得不好... 較無彈性...)
+# 因為中文版處理方式較麻煩 (需斷詞)，故分成兩種版本
+def read_json_en(keywords_of_title_en, keywords_of_text_en):
+    # 將所有爬到的 English新聞 們整合成一份 txt 檔案，以便將來討論某個特定關鍵字的成因
+    def aggregate_news_en(json_file):
+        with open('aggregated_news_en.txt', 'w') as f:
+            for data in json_file:
+                f.write('news_title: ')
+                f.write(str(data['news_title'].encode(encoding="utf-8")))
+                f.write('\n')
+                f.write('news_text: ')
+                f.write(str(data['news_text'].encode(encoding="utf-8")))
+                f.write('\n')
+                f.write('news_link: ')
+                f.write(str(data['news_link'].encode(encoding="utf-8")))
+                f.write('\n\n')
 
 
-# 讀取英文版本之json資料檔案，因為中文版之處理方式較麻煩 (需斷詞)，故分成兩種版本之函式
-def read_json_en():
-    # 我們要一次處理多個 json 檔案
-    file_name = ['diploma+inflation_0602', 'degree+inflation_0602', 'credential+inflation_0602', 'grade+inflation_0608', 'Academic+Inflation_0608', 'unemployed+after+graduation_0608']
-    json_data_list = list() # 把所有讀進來的JSON檔案存進一個list內
+    # 我們要一次處理多個 json 檔案，這邊要 ***手動*** 設定資料夾名稱，並放在project的同一個資料夾內 (或者用絕對路徑)
+    file_name = ['Anti+Extradition+Law_0622', 'Hong+Kong_0622', 'Tiananmen_0622']
+    # 把所有讀進來的JSON檔案存進一個list內
+    json_data_list = list()
     for name in file_name:
         if os.path.exists(name):
-            with open(name + '/' + 'search_result.json', 'r') as input_file:
+            print(name + '/' + name[:-5] + '.json')
+            with open(name + '/' + name[:-5] + '.json', 'r') as input_file:
                 tmp_json_data = json.load(input_file)
                 json_data_list.extend(tmp_json_data)
         else:
-            print('檔案名稱 %s 應該有誤，請檢查' % (name))
+            print('資料夾名稱: %s 應該有誤，請檢查' % (name))
     # print(json_data_list)
-    aggregate_news_en(json_data_list)
-    # print(len(json_data_list))  # 看一下讀進了幾條新聞
+    # print(len(json_data_list))      # 看一下讀進了幾條新聞
+    aggregate_news_en(json_data_list) # 將所有讀進來的 英文 新聞整合成一份txt檔
 
-    global title_keywords_en, text_keywords_en
-    title_keywords_en = []  # 所有標題關鍵字
-    text_keywords_en = []   # 所有內文摘要
+    # 開始把句子中的單字拆出來，順便去除stop_word，未來要做資料分析用
     for news in json_data_list:
-        # 首先處理新聞標題
+        # 處理標題
         tmp = []
         for char in news['news_title'].lower().split():  # 記得要一律轉成小寫，不然之後會分析不出frequent pattern
             if char not in stop_word_list:
                tmp.append(char)
-        title_keywords_en.append(tmp)
+        keywords_of_title_en.append(tmp)
 
         # 處理內文
         tmp = []
         for char in news['news_text'].lower().split():  # 記得要一律轉成小寫，不然之後會分析不出frequent pattern
             if char not in stop_word_list:
                 tmp.append(char)
-        text_keywords_en.append(tmp)
-
-    # print(title_keywords_en)
-    # print(text_keywords_en)
+        keywords_of_text_en.append(tmp)
 
 
-# 讀取中文版本之json資料檔案，因為中文版之處理方式較麻煩 (需斷詞)，故分成兩種版本之函式
-def read_json_ch():
-    # 我們要一次處理多個 json 檔案
-    file_name = ['學歷貶值_0608', '學歷通膨_0602', '學歷氾濫_0608', '學歷不值錢_0608', '畢業即失業_0608', '高學歷_0608']
+# 讀取 中文 版本JSON檔，並將所有檔案整合成一份 txt 檔並輸出，接著再做資料處理  (老實說，模組化寫得不好... 較無彈性...)
+# 因為中文版處理方式較麻煩 (需斷詞)，故分成兩種版本
+def read_json_ch(keywords_of_title_ch, keywords_of_text_ch):
+    # 將所有爬到的新聞們整合成一份 txt 檔案，中文版需處理編碼問題，比較麻煩
+    def aggregate_news_ch(json_file):
+        with open('aggregated_news_ch.txt', 'wb') as f:  # 這邊要記得用wb
+            for data in json_file:
+                f.write('news_title: '.encode('utf8'))
+                f.write(data['news_title'].encode('utf8'))
+                f.write('\n'.encode('utf8'))
+                f.write('news_text: '.encode('utf8'))
+                f.write(data['news_text'].encode(encoding="utf-8"))
+                f.write('\n'.encode('utf8'))
+                f.write('news_link: '.encode('utf8'))
+                f.write(data['news_link'].encode(encoding="utf-8"))
+                f.write('\n\n'.encode('utf8'))
+
+
+    # 我們要一次處理多個 json 檔案，這邊要 ***手動*** 設定資料夾名稱，並放在project的同一個資料夾內 (或者用絕對路徑)
+    file_name = ['六四_0622', '香港_0622', '反送中_0622']
     json_data_list = list()
     for name in file_name:
         if os.path.exists(name):
-            with open(name + '/' + 'search_result.json', 'r') as input_file:
+            with open(name + '/' + name[:-5] + '.json', 'r') as input_file:
                 tmp_json_data = json.load(input_file)
                 json_data_list.extend(tmp_json_data)
         else:
-            print('檔案名稱 %s 應該有誤，請檢查' % (name))
+            print('檔案名稱: %s 應該有誤，請檢查' % (name))
     # print(json_data_list)
-    aggregate_news_ch(json_data_list)
-    # print(len(json_data_list))
+    # print(len(json_data_list)) # 看一下讀進了幾條新聞
+    aggregate_news_ch(json_data_list)  # 將所有讀進來的 中文 新聞整合成一份txt檔
 
-    global title_keywords_ch, text_keywords_ch
-    title_keywords_ch = []
-    text_keywords_ch = []
+    # 開始把句子中的字拆出來 (中文斷詞)，順便去除stop_word，未來要做資料分析用
     for news in json_data_list:
-        # 處理新聞標題
+        # 處理標題
         title = news['news_title']                   # 中文比較麻煩，沒辦法用split來切。 需使用 結巴 函式庫
         title = Converter('zh-hans').convert(title)  # 繁體中文轉換成簡體中文，這樣斷詞的效果比較好
         # print(title)
@@ -112,7 +105,7 @@ def read_json_ch():
         # seg_list = jieba.cut_for_search(title, HMM=True)   # 搜尋引擎模式，好像沒有比較厲害?
         split_word = "/".join(seg_list)
         # print(split_word)
-        title_keywords_ch.append(split_word)
+        keywords_of_title_ch.append(split_word)
 
         # 處理內文
         text = news['news_text']                    # 中文比較麻煩，沒辦法用split來切。 需使用 結巴 函式庫
@@ -122,13 +115,11 @@ def read_json_ch():
         # seg_list = jieba.cut_for_search(text, HMM=True)   # 搜尋引擎模式，不知道有沒有比較厲害?
         split_word = "/".join(seg_list)
         # print(split_word)
-        text_keywords_ch.append(split_word)
+        keywords_of_text_ch.append(split_word)
 
-    # 將斷詞過後的中文字串弄成適合作Apriori的資料格式 (list)，故使用到tmp, tmp2
-    # print(title_keywords_ch)
-    # print(text_keywords_ch)
+    # 將斷詞過後的中文 標題 弄成適合作Apriori的資料格式 (list)，故使用到tmp, tmp2 (外層list包一堆內層list)
     tmp = []
-    for sentence in title_keywords_ch:
+    for sentence in keywords_of_title_ch:
         words = sentence.split('/')
         tmp2 = []
         for word in words:
@@ -136,10 +127,11 @@ def read_json_ch():
                 word = Converter('zh-hant').convert(word)  # 最後轉換回繁體中文
                 tmp2.append(word)
         tmp.append(tmp2)
-    title_keywords_ch = tmp
+    keywords_of_title_ch[:] = tmp # 這麼做超級重要!! 因為 keywords_of_text_ch[] = tmp 只是改變local的指標，當函數結束就被回收了 (所以不會真的變動到...)
 
+    # 將斷詞過後的中文 內文 弄成適合作Apriori的資料格式 (list)，故使用到tmp, tmp2 (外層list包一堆內層list)
     tmp = []
-    for sentence in text_keywords_ch:
+    for sentence in keywords_of_text_ch:
         words = sentence.split('/')
         tmp2 = []
         for word in words:
@@ -147,25 +139,24 @@ def read_json_ch():
                 word = Converter('zh-hant').convert(word) # 最後轉換回繁體中文
                 tmp2.append(word)
         tmp.append(tmp2)
-    text_keywords_ch = tmp
-    # 轉換結束
-    # print(title_keywords_ch)
-    # print(text_keywords_ch)
+    keywords_of_text_ch[:] = tmp # 這麼做超級重要!! 因為 keywords_of_text_ch[] = tmp 只是改變local的指標，當函數結束就被回收了 (所以不會真的變動到...)
 
 
-# 輸出frequent pattern
-def freq_pat_analy(data):
+# 回傳frequent pattern分析後的結果 (frozen set)
+def frequent_pattern_analyze(data):
+    print('----- 開始Frequent Pattern分析... (Apriori) -----')
     # print(data)
     association_rules = apriori(data)
     association_results = list(association_rules)
-    freq_pat_result = []
+    frequent_pattern_result = []
     for i in association_results:
-        freq_pat_result.append(i)
-    return freq_pat_result
+        frequent_pattern_result.append(i)
+    print('----- Frequent Pattern分析完成! -----')
+    return frequent_pattern_result
+
 
 # 由於Frequent Pattern效果不好，故嘗試關鍵字頻率分析
 def frequency_statistics(data):
-    # print(data)
     frequency_count = {}
     for news_title in data:
         for word in news_title:
@@ -174,21 +165,22 @@ def frequency_statistics(data):
             elif word in frequency_count and word not in stop_word_list:
                 frequency_count[word] += 1
 
-    # print(frequency_count)
+    # 依照出現頻率由大到小排序
     sorted_frequency_count = sorted(frequency_count.items(), key=lambda x: x[1], reverse=True)
+    # 這邊再進一步處理，把一些 單一字 或 頻率太低的字過濾掉 (e.g., '我': 14)
     freq_count_result = []
     for i in sorted_frequency_count:
-        if len(i[0]) >= 2 and i[1] >= 15:
+        if len(i[0]) >= 2 and i[1] >= 15:  # len(i[0]) 是中文詞長度、len(i[1]) 是出現頻率 (原則上我們對單一中文字沒興趣，故過濾)
             freq_count_result.append( (i[0], i[1]) )
-
     return freq_count_result
 
 
 # 方便 print list用的
 def print_data(data):
+    print(' --- print data... ---')
     for i in data:
         print(i)
-
+    print(' --- print finished! ---')
 
 # 將分析出來的 頻率統計 輸出成4個txt檔案，以供將來做成文字雲
 def output_frequency_count(word_count_title_en, word_count_text_en,
@@ -215,62 +207,64 @@ def output_frequency_count(word_count_title_en, word_count_text_en,
             f.write('\n')
 
 
-# 將統計出來的資料視覺化成文字雲 共4張圖片，不輸出frequent pattern (沒有明顯pattern, 且比較難處理)
-def text_cloud():
-    # 注意: 這邊是正常情況下的寫法，但我要手動處理一下資料，來讓圖片更好看，故將處理過後的txt檔案改成revised
-    # for file_name in ['word_count_title_en', 'word_count_text_en', 'word_count_title_ch', 'word_count_text_ch']:
-    for file_name in ['word_count_title_en_revised', 'word_count_text_en_revised', 'word_count_title_ch_revised', 'word_count_text_ch_revised']:
+# 將統計出來的資料視覺化成文字雲 共4張圖片，不輸出frequent pattern (沒有找到明顯的pattern, 且格式比較難處理)
+def output_word_cloud():
+    # 這邊要 ***手動*** 設定txt檔案名稱，並放在project的同一個資料夾內 (或者用絕對路徑)
+    for file_name in ['word_count_title_en', 'word_count_text_en', 'word_count_title_ch', 'word_count_text_ch']:
         text = open( file_name + ".txt", 'r').read()
         wc = WordCloud(background_color="white",
                        width=1000,
                        height=860,
                        margin=2,
-                       font_path='msyh.ttf',
-                       collocations=False, # 這行超她媽重要!!!，不然字體會重複出現多次
+                       font_path='msyh.ttf', # 引入一個支援中文的字型，建議字型放在同一個資料夾內
+                       collocations=False,   # 這行超重要!!!，不然字體會重複出現多次
                        max_words=100,
-                       max_font_size=200,
+                       max_font_size=150,
                        min_font_size=32)
         wc.generate(text)
         wc.to_file(file_name + '.png')
 
 
 if __name__ == '__main__':
-    read_json_en() # 讀取JSON檔案，並存進 title_keywords_en, text_keywords_en 兩個全域的list
-    read_json_ch() # 讀取JSON檔案，並存進 title_keywords_ch, text_keywords_ch 兩個全域的list
+    # 首先宣告list，未來要存新聞 標題&內文 關鍵詞用的
+    keywords_of_title_en, keywords_of_text_en = list(), list()
+    keywords_of_title_ch, keywords_of_text_ch = list(), list()
 
-    print('--------------- 開始英文版Frequent Pattern分析 ---------------')
-    freq_pat_of_title_en = freq_pat_analy(title_keywords_en)
-    print_data(freq_pat_of_title_en)
-    print(' - - - - - - - - - - - - - - - - - - - - - -  ')
-    freq_pat_of_text_en = freq_pat_analy(text_keywords_en)
-    print_data(freq_pat_of_text_en)
-    print('--------------- 英文版Frequent Pattern分析結束 ---------------')
-    print('--------------- 開始中文版Frequent Pattern分析 ---------------')
-    freq_pat_of_title_ch = freq_pat_analy(title_keywords_ch)
-    print_data(freq_pat_of_title_ch)
-    print(' ==========================================  ')
-    freq_pat_of_text_ch = freq_pat_analy(text_keywords_ch)
-    print_data(freq_pat_of_text_ch)
-    print('--------------- 中文版Frequent Pattern分析結束 ---------------')
+    # 讀取 English JSON檔案，並存進 keywords_of_title_en, keywords_of_text_en 兩個list
+    read_json_en(keywords_of_title_en, keywords_of_text_en)
+    print_data(keywords_of_title_en)
+    print_data(keywords_of_text_en)
 
-    print('--------------- 開始英文版 字母頻率 分析 ---------------')
-    freq_count_of_title_en = frequency_statistics(title_keywords_en)
-    print_data(freq_count_of_title_en)
-    print(' ==========================================  ')
-    freq_count_of_text_en = frequency_statistics(text_keywords_en)
-    print_data(freq_count_of_text_en)
-    print('--------------- 英文版 字母頻率 分析結束 ---------------')
-    print('--------------- 開始中文版 字母頻率 分析 ---------------')
-    freq_count_of_title_ch = frequency_statistics(title_keywords_ch)
-    print_data(freq_count_of_title_ch)
-    print(' ==========================================  ')
-    freq_count_of_text_ch = frequency_statistics(text_keywords_ch)
-    print_data(freq_count_of_text_ch)
-    print('--------------- 中文版 字母頻率 分析結束 ---------------')
+    # 讀取 中文 JSON檔案，並存進 keywords_of_title_ch, keywords_of_text_ch 兩個list
+    read_json_ch(keywords_of_title_ch, keywords_of_text_ch)
+    print_data(keywords_of_title_ch)
+    print_data(keywords_of_text_ch)
 
-    # 這行不要亂 call ，會蓋掉我好不容易調過的數據~
-    # print('------ 數據txt檔案輸出成功! ------')
-    # output_frequency_count(freq_count_of_title_en, freq_count_of_text_en,
-    #                        freq_count_of_title_ch, freq_count_of_text_ch)
-    print('------ 文字雲輸出成功! ------')
-    text_cloud()
+    # 開始做 英文版 Frequent Pattern 分析
+    frequent_pattern_of_title_en = frequent_pattern_analyze(keywords_of_title_en)
+    print_data(frequent_pattern_of_title_en)
+    frequent_pattern_of_text_en = frequent_pattern_analyze(keywords_of_text_en)
+    print_data(frequent_pattern_of_text_en)
+
+    # 開始做 中文版 Frequent Pattern 分析
+    frequent_pattern_of_title_ch = frequent_pattern_analyze(keywords_of_title_ch)
+    print_data(frequent_pattern_of_title_ch)
+    frequent_pattern_of_text_ch = frequent_pattern_analyze(keywords_of_text_ch)
+    print_data(frequent_pattern_of_text_ch)
+
+    # 開始做 English版 字彙頻率 分析
+    frequency_count_of_title_en = frequency_statistics(keywords_of_title_en)
+    print_data(frequency_count_of_title_en)
+    frequency_count_of_text_en = frequency_statistics(keywords_of_text_en)
+    print_data(frequency_count_of_text_en)
+
+    # 開始做 中文版 字彙頻率 分析
+    frequency_count_of_title_ch = frequency_statistics(keywords_of_title_ch)
+    print_data(frequency_count_of_title_ch)
+    frequency_count_of_text_ch = frequency_statistics(keywords_of_text_ch)
+    print_data(frequency_count_of_text_ch)
+
+    # 開始輸出 字母頻率 統計，共4個txt檔案
+    output_frequency_count(frequency_count_of_title_en, frequency_count_of_text_en, frequency_count_of_title_ch, frequency_count_of_text_ch)
+    # 開始輸出 文字雲 ，會讀取上一步驟之4個txt檔案 (或者你自己準備其他txt檔案也可以啦...)
+    output_word_cloud()
